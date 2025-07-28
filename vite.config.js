3be747da02922a1061b71b7fddad39880255d56f
OderoCeasar <156299730+OderoCeasar@users.noreply.github.com>
2025-07-28 23:19:07 +0300

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  // server: {
-  //   host: true,
-  //   allowedHosts: ["1b8f192f0333.ngrok-free.app"]
-  // }
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['c416e4e3a2fb.ngrok-free.app']
+  }
 });
